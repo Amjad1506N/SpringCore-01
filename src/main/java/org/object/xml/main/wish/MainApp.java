3e4ab0f96a30f7de9dded428202a993ee9f4c820
Amjad1506N <amjad786aei@gmail.com>
2020-06-08 09:57:50 +0530

    Spring IOC Create object Dynamically

---
+++ src/main/java/org/object/xml/main/wish/MainApp.java
@@ -0,0 +1,44 @@
+package org.object.xml.main.wish;
+
+import org.object.xml.wish.Wish;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+public class MainApp {
+     /*
+      * Here in this MainApp class we are creating Object for our IOC container to read config file
+      * and create object and manages the object of our classes and dependencies.
+      */
+	public static void main(String[] args) {
+		
+		Resource resource=null;
+		BeanFactory factory=null;
+		/*
+		 * Locates and load the Spring bean Configuration file from the specified location of the file 
+		 * System and keep it in ready State.
+		 */
+		resource=new FileSystemResource("src/main/java/org/object/xml/config/wish/applicationContext.xml");
+		/*
+		 * XmlBeanFactory is implementation class of BeanFactory(i) which is deprecated 
+		 * once the resource object assign to Container .The Container reads the file and checks
+		 * weather the given file properly well-form and valid or not
+		 * 
+		 *    1.Is it satisfied with spring given syntaxes then it is well formed
+		 *    1.IF its satisfied with XSD and DTD the it is valid
+		 *    
+		 *    The load the class and crate the object for classes that specified in Spring Configuration File
+		 */
+		factory=new XmlBeanFactory(resource);
+		
+		/*
+		 * Here getBean(-) return's the object 
+		 */
+		Wish wish=factory.getBean("wish",Wish.class);
+	
+	
+
+	}
+
+}
---
+++ src/main/java/org/object/xml/wish/Wish.java
@@ -0,0 +1,13 @@
+package org.object.xml.wish;
+
+public class Wish {
+	
+	/*
+	 * This below 0 param constructor is to prove Weather the Spring IOC CONTAINER 
+	 * is dynamically creating object for our classes or not.
+	 */
+	public Wish() {
+		System.out.println("Wish Object is Created");
+	}
+
+}
